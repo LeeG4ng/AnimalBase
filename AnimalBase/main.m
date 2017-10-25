5bd629aaf5117e44bbac2d14f8e88ef8d9d6e733
李刚 <LeeG4ng@qq.com>
2017-10-25 21:24:12 +0800

    Initial Commit

---
+++ AnimalBase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AnimalBase
+//
+//  Created by Gary Lee on 2017/10/25.
+//  Copyright © 2017年 LeeG4ng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
